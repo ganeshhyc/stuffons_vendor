bc2fcfa1d95ebf9963b3fa2ed96a59c755d36974
ganeshhyc <id.ganesh123@gmail.com>
2019-08-11 12:21:33 +0530

    some changes

---
+++ www/assets/js/animation.js
@@ -0,0 +1,9 @@
+setTimeout(()=>{
+    $('.onboarding-animation').animate({top:'35',height:'175px'},1000);
+    $('.stuffons').animate({top:'65px'},1000);
+    $('.stuffons-content').animate({top:'75'},1000);    
+    // console.log("working");
+},3000);
+setTimeout(()=>{
+    $('.onboarding-btn').css({'display':'block','text-align': 'center','margin':'auto'});
+},4000);
--- www/config/router.js
+++ www/config/router.js
@@ -3,8 +3,14 @@ app.config(function($routeProvider){
     .when('/',{
         templateUrl : 'application/view/landing.html'
     })
+    .when('/onboarding',{
+        templateUrl : 'application/view/register/onboarding_login_signup_button.html'
+    })
     .when('/profile',{
         templateUrl : 'application/view/profile.html'
+    })
+    .when('/signup',{
+        templateUrl : 'application/view/register/signup.html'
     })
     .when('/otp-signup',{
         templateUrl : 'application/view/register/signup_otp.html'
